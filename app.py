fe731d80648ae18d0d1f3e210ddf2099260a4f1e
khaldi abdarhmane <ha_khaldi@esi.dz>
2022-08-19 06:43:08 +0100

    25

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from bib import upload_my_file,hash_my_file
 import os
 import numpy as np
 # Keras
-#  --- from tensorflow.keras.preprocessing.image import load_img,img_to_array,array_to_img
-# --- from tensorflow.keras.models import Model,load_model
+from tensorflow.keras.preprocessing.image import load_img,img_to_array,array_to_img
+from tensorflow.keras.models import Model,load_model
 from werkzeug.utils import secure_filename
 # from gevent.pywsgi import WSGIServer
-# ---import mlflow
+import mlflow
 import warnings
 from fastapi import FastAPI, File, UploadFile
 import random
@@ -18,8 +18,8 @@ import shutil
 warnings.filterwarnings('ignore')
 # Define a flask app
 app = FastAPI()
-# ---- model= mlflow.keras.load_model("model_mlflow")
-# ----print(model.summary())
+model= mlflow.keras.load_model("model_mlflow")
+print(model.summary())
 # model._make_predict_function()          # Necessary
 
 
@@ -60,26 +60,27 @@ async def create_upload_file(file: UploadFile):
         shutil.copyfileobj(file.file, f_dest) 
         file_binary = open(file_dest, "rb").read()
         upload_my_file("dataset-farmy-pipline", "dataprod",file_binary , hash_my_file(file_dest).__str__()+".png")
-        return {"filename": hash_my_file(file_dest)}
+        return {"filename":  predict_class(file_dest) }
+        # hash_my_file(file_dest)
         ##################fin-step1#############################################
 
 
 
-# def prepare_image_for_prediction(img_path, size = (224,224)):
-#     # `img` is a PIL image of size 
-#     img = load_img(img_path, target_size=size)
-#     # `array` is a float32 Numpy array of shape (224, 224, 3)
-#     array = img_to_array(img)
-#     # We add a dimension to transform our array into a "batch"
-#     # of size (1, 224, 224, 3)
-#     array = np.expand_dims(array, axis=0)
-#     array = array/255
-#     return array
+def prepare_image_for_prediction(img_path, size = (224,224)):
+    # `img` is a PIL image of size 
+    img = load_img(img_path, target_size=size)
+    # `array` is a float32 Numpy array of shape (224, 224, 3)
+    array = img_to_array(img)
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, 224, 224, 3)
+    array = np.expand_dims(array, axis=0)
+    array = array/255
+    return array
 
-# def predict_class(image_path):
-#   image_array = prepare_image_for_prediction(image_path)
-#   result = model.predict(image_array)
-#   index_max = result.argmax(axis=1)[0]
-#   return index_max.__str__()
+def predict_class(image_path):
+  image_array = prepare_image_for_prediction(image_path)
+  result = model.predict(image_array)
+  index_max = result.argmax(axis=1)[0]
+  return index_max.__str__()
     
    
